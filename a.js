d69c758446efac4de72905e7a4842e026a061d4a
bacanic3210 <63243640+bacanic3210@users.noreply.github.com>
2024-08-13 12:44:20 +0900

    Add files via upload

--- a.js
+++ a.js
@@ -10,6 +10,8 @@ window.onload = function(){ // html 문서가 다 읽어진 후에 실행되는 
 
     input_id = document.getElementById("id");
     input_pw = document.getElementById("pw");
+    clock = document.getElementById("clock");
+    setInterval(out_time, 1000);
 
 //todo
 
@@ -35,6 +37,35 @@ function login(){
     }
 }
 
-function signup() {
+function out_time() {
+    var now = new Date();
+    str = now.getFullYear() + "년 " + (now.getMonth() + 1) + "월 " + now.getDate() + "일 ";
+    switch (now.getDay()) {
+        case 0:
+            str = str + "일요일 ";
+            break;
+        case 1:
+            str = str + "월요일 ";
+            break;
+        case 2:
+            str = str + "화요일 ";
+            break;
+        case 3:
+            str = str + "수요일 ";
+            break;
+        case 4:
+            str = str + "목요일 ";
+            break;
+        case 5:
+            str = str + "금요일 ";
+            break;
+        case 6:
+            str = str + "토요일 ";
+            break;
+    }
+    str = str + "<br>";
+    str = str + now.getHours() + "시 " + now.getMinutes() + "분 " + now.getSeconds() + "초 " + now.getMilliseconds() + "밀리초<br>";
 
+    clock.innerHTML = str;
 }
+
